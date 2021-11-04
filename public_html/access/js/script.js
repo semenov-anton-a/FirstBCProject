5b428591338ba92d31a6beb933aecf58d66e7b9a
Anton Semenov <semenov.anton.a@gmail.com>
2021-11-04 12:11:06 +0200

    NaviMenu DONE

--- public_html/access/js/script.js
+++ public_html/access/js/script.js
@@ -4,6 +4,10 @@ const navigation = {
     state : false,
     navi: document.querySelector("nav#navi"),
     btn : document.querySelectorAll("nav#navi i"),
+    closeMenu : function( ) {
+        this.navi.setAttribute('data-active', false);
+        this.state = false;
+    },
     click : function(){
         let curretState = Boolean(this.navi.getAttribute('data-active'));
         this.state = ( curretState != this.state );
@@ -12,10 +16,21 @@ const navigation = {
     init : function(){
         this.btn[0].addEventListener("click", ()=>{this.click()});
         this.btn[1].addEventListener("click", ()=>{this.click()});
+        window.addEventListener('resize', function(){
+            if( window.screen.width >= 767 ){ navigation.closeMenu(); }
+        });
         return false;
     },
-}
+}//Obj navigation END
+
+
 navigation.init();
 /*
  *  NAVIGATION END
  *****************/
+
+
+/**
+ *  event window resize */
+
+
