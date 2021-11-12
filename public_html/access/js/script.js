7b0a8e50604bd816ca8ca19fe1d5be5fe9caace9
Anton Semenov <semenov.anton.a@gmail.com>
2021-11-12 15:49:21 +0200

    Revert "Merge branch 'development'"
    This reverts commit c6942f82cfdbd7fb7eb1cf0b194fd1c0152f6bff, reversing
    changes made to bafefcfa46d475efc283d2ede53292cc4f6efb96.

--- public_html/access/js/script.js
+++ public_html/access/js/script.js
@@ -16,8 +16,8 @@ const navigation = {
     init : function(){
         this.btn[0].addEventListener("click", ()=>{this.click()});
         this.btn[1].addEventListener("click", ()=>{this.click()});
-        window.addEventListener('resize', ()=>{
-            if( window.screen.width >= 767 ){ this.closeMenu(); }
+        window.addEventListener('resize', function(){
+            if( window.screen.width >= 767 ){ navigation.closeMenu(); }
         });
     },
 }//Obj navigation END
